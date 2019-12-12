474e9efcf15e9226aee25b2e641b0fa164a9b8a1
Vojkovic Fran <vofran@pr4-09.studenti.math.hr>
2019-12-12 13:57:54 +0100

    beta

--- test.cpp
+++ test.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <list>
+#include <stack>
 
 using namespace std;
 
+void f(stack<int> &s){
+  s.push(5);
+}
 
 
 int main(){
-  vector< vector<int> > map;
-
-  map.resize(5);
-  for( int i = 0 ; i < map.size() ; ++i )
-    map[i].resize(4);
-    map[4][3]=9;
-    cout<<map[4][3]<<endl;
+  stack<int> s;
+  f(s);
+  cout<< s.top()<<endl;
   return 0;
 }
